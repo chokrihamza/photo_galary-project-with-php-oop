6f5cbccf37737bb73fdc5a85a8e46f4dc1285847
chokrihamza <chokrihamza1994@gmail.com>
2021-06-15 10:45:21 +0200

    finish logout page

--- admin/login.php
+++ admin/login.php
@@ -12,7 +12,7 @@ if (isset($_POST['submit'])) {
 
 
   //Method to check databae user
-  $user_found = User::verify_user($username, $passowrd);
+  $user_found = User::verify_user($username, $password);
   if ($user_found) {
     $session->login($user_found);
     redirect("index.php");
@@ -32,13 +32,13 @@ if (isset($_POST['submit'])) {
   <form id="login-id" action="" method="post">
 
     <div class="form-group">
-      <label for="username">Username</label>
+      <label for="username" style="color:wheat">Username</label>
       <input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>">
 
     </div>
 
     <div class="form-group">
-      <label for="password">Password</label>
+      <label for="password" style="color:wheat">Password</label>
       <input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
 
     </div>
---
+++ admin/logout.php
@@ -0,0 +1,5 @@
+<?php require_once('includes/header.php') ?>
+<?php
+$session->logout();
+redirect('login.php');
+?>
